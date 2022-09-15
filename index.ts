068d6e378f3c68ac94b8aca0452eaffff820bf1b
TheCoderOfBville <TheCoderOfBville@users.noreply.github.com>
2022-09-14 20:35:18 -0500

    1 file modified, 1 deleted

--- index.ts
+++ index.ts
@@ -1,6 +1,25 @@
-// Import stylesheets
-import './style.css';
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+function canFill(onHand:number, pending:number, requested:number):boolean {
+  try {
+    if(requested<1) {
+      throw new Error("Requested cannot be less than 1");
+    }
+    if(onHand<0 || pending<0) {
+      throw new Error("Values cannot be negative");
+    }
+    const able = onHand - pending -requested >= 0;
+    console.log("Can fill: " + able);
+  } catch(error) {
+    console.log(error.message);
+    return false;
+  } 
+}
+
+function test():void {
+  canFill(10, 5, 5);
+  canFill(10, 11, 1);
+  canFill(-1, 1, 0);
+  canFill(-1, 1, -1);
+}
+
+test();
